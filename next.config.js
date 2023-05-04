c9b95f81e2cbc2d5c67c504ba6ad332cc2d96b6e
Hernan Foix <hernan_foix@hotmail.com>
2023-05-03 23:30:45 -0300

    another way to fix it

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    API_URL: process.env.API_URL,
-  },
+  // env: {
+  //   API_URL: process.env.API_URL,
+  // },
 };
 
 module.exports = nextConfig;
